bf7ac907c2a09c7d86b12d8fd23e1e0741b58180
Bhupeshpanwar <155254153+Bhupeshpanwar@users.noreply.github.com>
2025-07-08 15:30:05 +0530

    Update dict.js
    for vercel

--- src/dict.js
+++ src/dict.js
@@ -1,22 +1,23 @@
 const soundnumbers = {
-    "0": "./public/sounds/0.mp3.mp3",
-    "1": "./public/sounds/1.mp3.mp3",
-    "2": "./public/sounds/2.mp3.mp3",
-    "3": "./public/sounds/3.mp3.mp3",
-    "4": "./public/sounds/4.mp3.mp3",
-    "5": "./public/sounds/5.mp3.mp3",
-    "6": "./public/sounds/6.mp3.mp3",
-    "7": "./public/sounds/7.mp3.mp3",
-    "8": "./public/sounds/8.mp3.mp3",
-    "9": "./public/sounds/9.mp3.mp3",
-    "+": "./public/sounds/plus.mp3.mp3",
-    "-": "./public/sounds/minus.mp3",
-    "/": "./public/sounds/divide.mp3.mp3",
-    "*": "./public/sounds/multiply.mp3",
-    ".":"./public/sounds/dot.mp3",
-    "clear":"./public/sounds/clear.mp3.mp3",
-    "Backspace":"./public/sounds/Backspace.mp3.mp3",
-    "Equals":"./public/sounds/Equals.mp3.mp3"
+   
+  "0": "/sounds/0.mp3.mp3",
+  "1": "/sounds/1.mp3.mp3",
+  "2": "/sounds/2.mp3.mp3",
+  "3": "/sounds/3.mp3.mp3",
+  "4": "/sounds/4.mp3.mp3",
+  "5": "/sounds/5.mp3.mp3",
+  "6": "/sounds/6.mp3.mp3",
+  "7": "/sounds/7.mp3.mp3",
+  "8": "/sounds/8.mp3.mp3",
+  "9": "/sounds/9.mp3.mp3",
+  "+": "/sounds/plus.mp3.mp3",
+  "-": "/sounds/minus.mp3",
+  "/": "/sounds/divide.mp3.mp3",
+  "*": "/sounds/multiply.mp3",
+  ".": "/sounds/dot.mp3",
+  "clear": "/sounds/clear.mp3.mp3",
+  "Backspace": "/sounds/Backspace.mp3.mp3",
+  "Equals": "/sounds/Equals.mp3.mp3"
 
     
 };
